56d71d3f0b588a08e91594f6adcba5857a9c8718
Perkles <jade.aabinajm@gmail.com>
2023-06-07 14:44:14 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+Java snake game 🐍
+
+
+>Game com intutio de praticar logica, loops e if/else.
---
+++ src/PythonGame.java
@@ -0,0 +1,14 @@
+public class PythonGame {
+
+    public static void main(String[] args) {
+
+        new GameFrame();
+
+
+
+
+
+
+
+    }
+}
